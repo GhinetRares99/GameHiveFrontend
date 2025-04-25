40fc5087ac930dd5b5857184cd6e672f2c6bc913
Ghinet Rares <rares.ghinet@yahoo.com>
2025-04-25 03:15:40 +0300

    Added an additional guard  for some pages

---
+++ src/app/guards/sign-in.guard.ts
@@ -0,0 +1,25 @@
+import { isPlatformBrowser } from '@angular/common';
+import { inject, PLATFORM_ID } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+
+export const signInGuard: CanActivateFn = (route, state) => {
+  const router = inject(Router);
+  const platformId = inject(PLATFORM_ID);
+
+  if (isPlatformBrowser(platformId)) {
+    const token = sessionStorage.getItem("authToken");
+    const role = sessionStorage.getItem("role");
+    
+    if(token != null && role != null)
+    {
+      return true;
+    }
+    else
+    {
+      router.navigate(['/signin']);
+      return false;
+    }
+  }
+
+  return false;
+};
